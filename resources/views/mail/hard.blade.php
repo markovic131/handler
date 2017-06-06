536bc5136bb943daf8ea84d46d347474d07d0ffa
Marko Aleksic <psybaron@gmail.com>
2017-06-06 16:17:10 +0200

    testing message headers decoding

--- resources/views/mail/hard.blade.php
+++ resources/views/mail/hard.blade.php
@@ -1,3 +1,12 @@
 <div>
-    <?php print_r($data); ?>
+    <b>Код:</b> {{ $data['code'] }}<br>
+    <b>Примател:</b> {{ $data['recipient'] }}<br>
+    <p>
+        <b>Грешка:</b> {{ $data['error'] }}
+    </p>
+    <hr>
+    <p>
+        <b>Додатни информации:</b><br>
+        {{ $data['message-headers'] }}
+    </p>
 </div>
